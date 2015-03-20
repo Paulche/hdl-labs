85453839a4639070f3aac2ce9a93ade4612e569b
Paul Chechetin <paulche@yandex.ru>
2015-03-20 20:37:47 +0300

    Finallyze beh for lab1

--- lab1/src/beh.vhd
+++ lab1/src/beh.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity device1 is 
   port(a, x42, x41, x32, x31, x22, x21, x12, x11, not_e: in std_logic; y4, y3, y2, y1: out std_logic);
-end device0;
+end device1;
 
 architecture BEH of device1 is 	
   signal v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10: std_logic;
@@ -11,20 +11,20 @@ architecture BEH of device1 is
 begin
   process(a,not_e,x42,x41,x32,x31,x22,x21,x12,x11)
   begin
-    if (x = '0') then
+    if (not_e = '0') then
       if (a = '0') then
-        y4 := x41;
-        y3 := x31;
-        y2 := x21;
-        y1 := x11;
+        y4 <= x41;
+        y3 <= x31;
+        y2 <= x21;
+        y1 <= x11;
       else 
-        y4 := x42;
-        y3 := x32;
-        y2 := x22;
-        y1 := x12;
+        y4 <= x42;
+        y3 <= x32;
+        y2 <= x22;
+        y1 <= x12;
       end if;
     else 
-      y1 := 0; y2 = 0; y3 = 0; y4 = 0;
+      y1 <= '0'; y2 <= '0'; y3 <= '0'; y4 <= '0';
     end if;
   end process;
 end BEH;
@@ -32,8 +32,8 @@ end BEH;
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity device0_tb is 
-end device0_tb;
+entity device1_tb is 
+end device1_tb;
 
 architecture BEH of device1_tb is
   component device0
